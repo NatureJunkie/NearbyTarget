abcae454ea5cbe792562d11eecb2e681af4f45f1
NatureJunkie <146461292+NatureJunkie@users.noreply.github.com>
2025-10-11 11:19:23 -0700

    Add Buttons.lua and close x on all the interfaces
    Buttons.lua is where you define your functions/ basically LUA macros/if you want to create a LUA 1 button macro this is where you put it

---
+++ NearbyTarget/Buttons.lua
@@ -0,0 +1,88 @@
+
+--Nearby Targets buttons
+function Button1()
+    print("Button1")
+
+end
+function  Button2()
+    print("Button2")
+
+end
+function Button3()
+    print("Button3")
+
+end
+function  Button4()
+    print("Button4")
+
+end
+function  Button5()
+
+    print("Button5")
+end
+function  Button6()
+    print("Button6")
+
+end
+function  Button7()
+
+    print("Button7")
+end
+--Raid Targets buttons
+function RButton1()
+    print("Button1")
+
+end
+function  RButton2()
+    print("Button2")
+
+end
+function RButton3()
+    print("Button3")
+
+end
+function  RButton4()
+    print("Button4")
+
+end
+function  RButton5()
+
+    print("Button5")
+end
+function  RButton6()
+    print("Button6")
+
+end
+function  RButton7()
+
+    print("Button7")
+end
+--Party Targets buttons
+function PButton1()
+    print("Button1")
+
+end
+
+function  PButton2()
+    print("Button2")
+end
+function PButton3()
+    print("Button3")
+
+end
+function  PButton4()
+    print("Button4")
+
+end
+function  PButton5()
+
+    print("Button5")
+end
+function  Button6()
+    print("Button6")
+
+end
+function  PButton7()
+
+    print("Button7")
+end
