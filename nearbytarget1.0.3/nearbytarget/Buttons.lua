c6b33b2588b39ecff8dac04045274f3e8b0c397b
NatureJunkie <146461292+NatureJunkie@users.noreply.github.com>
2025-10-16 04:26:23 -0700

    make a file

---
+++ nearbytarget1.0.3/nearbytarget/Buttons.lua
@@ -0,0 +1,127 @@
+--=========================================
+-- Nearby Target Buttons
+--=========================================
+function Button1()  
+  print("button1") 
+end
+function Button2() 
+  print("button2") 
+end
+function Button3()  
+  print("button3") 
+end
+function Button4()  
+  print("button4")
+end
+function Button5() 
+  print("button5") 
+end
+function Button6() 
+  print("button6")
+end
+function Button7() 
+  print("button7")
+end
+--=========================================
+-- Raid Target Buttons
+--=========================================
+function RaidButton1()
+    print("RaidButton1")
+end
+function RaidButton2()
+    print("RaidButton2")
+end
+function RaidButton3()
+    print("RaidButton3")
+end
+function RaidButton4()
+    print("RaidButton4")
+end
+function RaidButton5()
+    print("RaidButton5")
+end
+function RaidButton6()
+    print("RaidButton6")
+end
+function RaidButton7()
+    print("RaidButton7")
+end
+function RaidButton8()
+    print("RaidButton8")
+end
+function RaidButton9()
+    print("RaidButton9")
+end
+function RaidButton10()
+    print("RaidButton10")
+end
+function RaidButton11()
+    print("RaidButton11")
+end
+function RaidButton12()
+    print("RaidButton12")
+end
+function RaidButton13()
+    print("RaidButton13")
+end
+function RaidButton14()
+    print("RaidButton14")
+end
+function RaidButton15()
+    print("RaidButton15")
+end
+function RaidButton16()
+    print("RaidButton16")
+end
+
+--=========================================
+-- Party Target Buttons
+--=========================================
+function PartyButton1()
+    print("PartyButton1")
+end
+function PartyButton2()
+    print("PartyButton2")
+end
+function PartyButton3()
+    print("PartyButton3")
+end
+function PartyButton4()
+    print("PartyButton4")
+end
+function PartyButton5()
+    print("PartyButton5")
+end
+function PartyButton6()
+    print("PartyButton6")
+end
+function PartyButton7()
+    print("PartyButton7")
+end
+function PartyButton8()
+    print("PartyButton8")
+end
+function PartyButton9()
+    print("PartyButton9")
+end
+function PartyButton10()
+    print("PartyButton10")
+end
+function PartyButton11()
+    print("PartyButton11")
+end
+function PartyButton12()
+    print("PartyButton12")
+end
+function PartyButton13()
+    print("PartyButton13")
+end
+function PartyButton14()
+    print("PartyButton14")
+end
+function PartyButton15()
+    print("PartyButton15")
+end
+function PartyButton16()
+    print("PartyButton16")
+end
